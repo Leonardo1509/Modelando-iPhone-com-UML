13d593b54f4ea93cd074c823f0fd32095aaad8be
Leonardo da Silva <leonardo.leo2021@gmail.com>
2024-06-24 21:28:17 -0300

    first commit

---
+++ src/iPhone.java
@@ -0,0 +1,74 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    private String modelo;
+    private String numeroDeSerie;
+
+    public iPhone(String modelo, String numeroDeSerie) {
+        this.modelo = modelo;
+        this.numeroDeSerie = numeroDeSerie;
+    }
+
+    // Métodos da interface ReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    // Métodos da interface AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo a chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando o correio de voz...");
+    }
+
+    // Métodos da interface NavegadorInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    // Getters e setters
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getNumeroDeSerie() {
+        return numeroDeSerie;
+    }
+
+    public void setNumeroDeSerie(String numeroDeSerie) {
+        this.numeroDeSerie = numeroDeSerie;
+    }
+}
